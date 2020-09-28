89fdcc76bc7899e4acb84632482e9e0e89319a9a
ümit çelebi <53602829+umitcelebi@users.noreply.github.com>
2020-09-28 12:36:08 +0300

    Update UserDetailsServiseImpl.java

--- src/main/java/com/ucelebi/security/UserDetailsServiseImpl.java
+++ src/main/java/com/ucelebi/security/UserDetailsServiseImpl.java
@@ -9,18 +9,18 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.ucelebi.model.User;
-import com.ucelebi.repo.UserRepository;
+import com.ucelebi.service.UserService;
 
 @Service
 public class UserDetailsServiseImpl implements UserDetailsService{
 
 	@Autowired
-	UserRepository userRepository;
+	UserService userService;
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
-		Optional<User> user=userRepository.findByUsername(username);
+		Optional<User> user=userService.findByUsername(username);
 		
 		user.orElseThrow(()->new UsernameNotFoundException("Not Found: "+username));
 		
