55ea7e144bb41bf6aada8926b2e205b53db21d04
ümit çelebi <53602829+umitcelebi@users.noreply.github.com>
2020-09-28 12:28:06 +0300

    Update FileService.java

--- src/main/java/com/ucelebi/service/FileService.java
+++ src/main/java/com/ucelebi/service/FileService.java
@@ -3,34 +3,13 @@ package com.ucelebi.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ucelebi.model.File;
-import com.ucelebi.repo.FileRepository;
 
-@Service
-public class FileService {
-	
-	@Autowired
-	private FileRepository fileRepository;
-	
-	public File saveFile(MultipartFile multipartFile) {
-		String fileName=multipartFile.getOriginalFilename();
-		
-		try {
-			File file=new File(fileName, multipartFile.getContentType(), multipartFile.getBytes());
-			return fileRepository.save(file);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-	public Optional<File> getFile(int id){
-		return fileRepository.findById(id);
-	}
-	public List<File> getFiles(){
-		return fileRepository.findAll();
-	}
+public interface FileService {
+
+	File saveFile(MultipartFile multipartFile);
+	Optional<File> getFile(int id);
+	List<File> getFiles();
 }
